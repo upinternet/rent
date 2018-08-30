bfd034e0af61c97b068cfaf9f190a7669ad062c6
heyanhui <he.yanhui2008@163.com>
2018-08-30 18:06:38 +0800

    ease rent

---
+++ src/main/java/com/hyh/ease/rent/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.hyh.ease.rent.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @RequestMapping(value = "/detail/{id}" , method = RequestMethod.GET)
+    public String getUserName(@PathVariable String id)
+    {
+        return id;
+    }
+
+}
